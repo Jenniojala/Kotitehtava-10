65807eebdaf11cca71491da4d55a5da056799b37
Jenniojala <78203840+Jenniojala@users.noreply.github.com>
2021-02-05 12:35:08 +0200

    julkaisuversio

---
+++ kotitehtava/footer.php
@@ -0,0 +1,3 @@
+<?php
+echo "<p>&copy; Jenni Ojala " . date("Y") . " </p>";
+?>
---
+++ kotitehtava/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fi">
+<head>
+<meta charset="utf-8">
+<title>Kotitehtävä 10</title>
+<style>
+h1 {color:#CD9B9A; text-align: center;}
+p {font-size: 20px; text-align:center; font-family: Arial, Helvetica, sans-serif; text-align: center;}
+</style>
+</head>
+<body>
+<?php include 'header.php';?>
+
+<p>Tervetuloa kotitehtävään 10</p>
+<p>PHP:n require ja include komentojen harjoitus</p>
+
+<?php include 'footer.php';?>
+
+</body>
+</html>
